dbe975d4927295a032976bb7ee6898fa1bcf6b1e
jamess-sudo <aj369369aj@gmail.com>
2026-03-17 18:56:53 +0400

    Add files via upload

---
+++ Düzbucaqlının sahəsinin hesablanması/Proyekt/Source.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	string ad;
+	int en, uzunluq, sahe;
+	/*cout << "Tanis olmaq olar?:";
+	cin >> ad;
+	cout << "Salam " << ad << " necesen?" << endl;
+    */
+	cout << "Duzbucaqlinin enini girin:";
+	cin >> en;
+
+	cout << "Duzbucaqlinin uzunlugunu girin:";
+	cin >> uzunluq;
+
+	cout << "Duzbucaqlinin sahesi:" << en * uzunluq << endl;
+	
+	return 0;
+
+
+
+
+}
